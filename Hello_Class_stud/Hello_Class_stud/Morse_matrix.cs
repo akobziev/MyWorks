36d4c30a9a4d58e92ebc86178054e612aac3f0fa
akobziev <Kobziev.aa@gmail.com>
2018-11-18 01:39:26 +0200

    test

---
+++ Hello_Class_stud/Hello_Class_stud/Morse_matrix.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace Hello_Class_stud
+{
+    //Implement class Morse_matrix derived from String_matrix, which realize IMorse_crypt
+    class Morse_matrix : String_matrix, IMorse_crypt
+    {
+        public const int Size_2 = Alphabet.Size;
+        private int offset_key = 0;
+
+
+        //Implement Morse_matrix constructor with the int parameter for offset
+        //Use fd(Alphabet.Dictionary_arr) and sd() methods
+
+        public Morse_matrix(){}
+        public Morse_matrix(int offset)
+        {
+            offset_key = offset;
+        }
+
+        //Implement Morse_matrix constructor with the string [,] Dict_arr and int parameter for offset
+        //Use fd(Dict_arr) and sd() methods
+        public Morse_matrix(string [,] dict_arr, int offset)
+        {
+            offset_key = offset;
+            fd(dict_arr);
+            sd();
+        }
+
+        //Implement Morse_matrix operator +
+
+        //Realize crypt() with string parameter
+        //Use indexers        
+        public string Crypt(string stringToCrypt)
+        {            
+            var sb = new StringBuilder();
+            foreach (var c in stringToCrypt)
+            {
+                for (int i = 0; i < Alphabet.Size; i++)
+                {
+                    if (c == Alphabet.Dictionary_arr[0, i].ToCharArray()[0])
+                    {
+                        sb.Append(Alphabet.Dictionary_arr[1, i]);
+                        break;
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+        
+        //Realize decrypt() with string array parameter
+        //Use indexers
+        public string Decrypt(string[] stringsToDecrypt)
+        {
+            var sb = new StringBuilder();
+            foreach (var str in stringsToDecrypt)
+            {
+                for (int i = 0; i < Alphabet.Size; i++)
+                {
+                    if (str == Alphabet.Dictionary_arr[1, i])
+                    {
+                        sb.Append(Alphabet.Dictionary_arr[0, i]);
+                        break;
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+
+        //Implement Res_beep() method with string parameter to beep the string
+        public void Res_beep(string result)
+        {
+            foreach (var c in result)
+            {
+                if (c == '.')
+                {
+                    Console.Beep(650, 100);
+                    Thread.Sleep(200);
+                }
+                else if (c == '-')
+                {
+                    Console.Beep(650, 400);
+                    Thread.Sleep(200);
+                }
+            }
+        }
+
+
+        private void fd(string[,] Dict_arr)
+        {
+            for (int ii = 0; ii < Size_2; ii++)
+                for (int jj = 0; jj < Size_2; jj++)
+                    this[ii, jj] = Dict_arr[ii, jj];
+        }
+
+
+        private  void sd()
+        {
+            int off = Size_2 - offset_key;
+            for (int jj = 0; jj < off; jj++)
+                this[1, jj] = this[1, jj + offset_key];
+            for (int jj = off; jj < Size_2; jj++)
+                this[1, jj] = this[1, jj - off];
+        }
+
+
+    }
+}
+
--- Hello_Class_stud/Hello_Class_stud/Properties/AssemblyInfo.cs
+++ Hello_Class_stud/Hello_Class_stud/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyWorks")]
+[assembly: AssemblyTitle("Hello_Class_stud")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyWorks")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Hello_Class_stud")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2bbb032c-6f00-444e-b0c8-5b56986b5eb7")]
+[assembly: Guid("a31500d5-4bf0-46b1-ab4f-1cfee1680dde")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
