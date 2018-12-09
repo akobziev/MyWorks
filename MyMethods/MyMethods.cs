4640e02fb1dab95e9de55485749e23ee03616c8b
akobziev <Kobziev.aa@gmail.com>
2018-12-09 22:03:45 +0200

    [add] MyMethods.cs

---
+++ MyMethods/MyMethods.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMethods
+{
+    public static class MyMethods
+    {
+        internal static T Find<T>(T[] values, Predicate<T> predicate)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (predicate(values[i]))
+                {
+                    return values[i];
+                }
+            }
+            return default(T);
+        }
+
+        internal static T FindLast<T>(T[] values, Predicate<T> predicate)
+        {
+            int index = -1;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (predicate(values[i]))
+                {
+                    index = i;
+                }
+            }            
+            return index >= 0? values[index] : default(T);
+        }
+
+        internal static List<T> FindAll<T>(T[] values, Predicate<T> predicate)
+        {
+            List<T> result = new List<T>();
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (predicate(values[i]))
+                {
+                    result.Add(values[i]);
+                }
+            }
+            return result;
+        }
+
+        internal static bool TrueForAll<T>(T[] values, Predicate<T> predicate)
+        {
+            int counter = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (predicate(values[i]))
+                {
+                    counter++;
+                }
+            }
+            return counter == values.Length ? true : false;
+        }
+
+        internal static bool Exists<T>(T[] v, Predicate<T> predicate)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (predicate(v[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        internal static int FindLastIndex<T>(T[] values, Predicate<T> predicate)
+        {
+            int index = -1;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (predicate(values[i]))
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        internal static int FindIndex<T>(T[] values, Predicate<T> predicate)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (predicate(values[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ MyMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyMethods.Find(new[] { "Test", "Vasa" }, el => el.Contains("s")));
+            Console.WriteLine(MyMethods.FindLast(new[] { "Vasa", "Kate", "Test" }, el => el.Contains("t")));
+            Console.WriteLine(MyMethods.FindIndex(new[] { "Vasa", "Kate", "Test" }, el => el.ToLower().Contains("t")));
+            Console.WriteLine(MyMethods.FindLastIndex(new[] { "Vasa", "Kate", "Test" }, el => el.ToLower().Contains("s")));
+            MyMethods.FindAll(new[] { "Vasa", "Kate", "Test" }, el => el.ToLower().Contains("s")).ForEach(el => Console.WriteLine(el));
+            Console.WriteLine(MyMethods.Exists(new[] { "Vasa", "Kate", "Test" }, el => el.ToLower().Contains("m")));
+            Console.WriteLine(MyMethods.TrueForAll(new[] { "Vasae", "Kates", "Test" }, el => el.ToLower().Contains("s")));
+        }
+    }
+}
