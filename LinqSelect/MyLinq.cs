d24b2917f69f844bd5b1d3bfa0b9c65e230d06a3
AntonKobziev <kobziev.aa@gmail.com>
2018-12-14 18:39:43 +0200

    [add] LinqSelect

---
+++ LinqSelect/MyLinq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSelect
+{
+    public static class MyLinq
+    {
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            foreach (var s in source)
+            {
+                yield return selector(s);
+            }
+        }
+    }
+}
---
+++ LinqSelect/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSelect
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var number in new[] { "adfasf", "sdfdf", "dfdfv", "aaaadf" }.MySelect(s => s.ToUpper()))
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static IEnumerable<string> GetWithoutVowels(IEnumerable<string> strings)
+        {
+            var pattern = new HashSet<char>("aeiouy".ToCharArray());
+            return strings.Select(s => s).Where(s => s.All(c => !pattern.Contains(c)));
+        }
+
+        private static IEnumerable<int> GetFloatToInt(IEnumerable<float> floats)
+        {
+            return floats.Select(f => (int) f);
+        }
+
+        private static IEnumerable<bool> GetBoolSequence(IEnumerable<int> nums)
+        {
+            return nums.Select(n => n > 0);
+        }
+
+        private static IEnumerable<int> PositiveNumbers(IEnumerable<int> nums)
+        {
+            return nums.Select(n => n < 0? -n: n);
+        }
+    }
+}
