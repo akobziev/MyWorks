1d88f079eb866da464649548c02096e18c5c24f6
akobziev <Kobziev.aa@gmail.com>
2018-12-16 23:42:04 +0200

    [add] LinqHomeTask1.csproj

---
+++ LinqHomeTask1/MyLastLinq.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqHomeTask1
+{
+    public static class MyLastLinq
+    {
+        public static IEnumerable<TResult> MySelectMany<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            List<TResult> res = new List<TResult>();
+            foreach (var item in source)
+            {
+                res.AddRange(selector(item));
+            }
+            return (IEnumerable<TResult>)res;
+        }
+
+        //public static IEnumerable<IGrouping<TKey, TSource>> MyGroupBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+        //{
+        //    foreach (var item in source)
+        //    {
+        //        var res = new KeyValuePair<TKey, TSource>(selector(item), item);
+        //        yield return res;
+        //    }
+        //}
+    }
+}
---
+++ LinqHomeTask1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinqHomeTask1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //WorkWithPaging();
+            List<string> vs = new List<string> {"afdsf", "fdasff", "fffffdasf", "fffffdasf" };
+            var res = vs.MySelectMany(s => s.ToUpper()).Where(c => c != 'A').ToList();
+
+            var res1 = vs.GroupBy(s => s.Length);
+            foreach (var item in res1)
+            {
+                Console.WriteLine(item.Key);
+                foreach (var c in item)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+            var res2 = vs.Aggregate((acc, s) => acc + s);
+            foreach (var item in res2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        #region Paging
+
+        private static void WorkWithPaging()
+        {
+            foreach (var item in Paging(new[] { "a", "b", "c", "d", "e", "i" }, 2, 3).ToList())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static IEnumerable<T> Paging<T>(IEnumerable<T> array, int elementsOnPage, int currPage)
+        {
+            return array.Skip(elementsOnPage * (currPage - 1)).Take(elementsOnPage);
+        }
+
+        #endregion
+    }
+}
